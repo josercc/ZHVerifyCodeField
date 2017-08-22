6529802e410e7f40e0f76980144db9ffc2016276
15038777234 <15038777234@163.com>
2017-08-22 17:32:29 +0800

    初始化工程

---
+++ ZHVerifyCodeFiled/ZHVerifyCodeFiled/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHVerifyCodeFiled
+//
+//  Created by 张行 on 2017/8/22.
+//  Copyright © 2017年 张行. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
